ef293a4a44bc1ce71b3a307473b001760142b0b6
yolanda <y.llop@usp.ceu.es>
2023-11-28 16:30:07 +0100

    graf

--- README.md
+++ README.md
@@ -1,23 +1,43 @@
-README - Algoritmo A* en Java
-Descripción del Proyecto
-El propósito de esta práctica es que el alumno reutilice código que implementa el algoritmo A*. Se bajará código de Github, y lo utilizará para ejecutareste algoritmo para un grafo concreto.
+# Páctica de búsqueda heurística sin adversarios
 
-Estructura del Proyecto
-El proyecto consiste en un programa Java con las siguientes partes:
+El propósito de esta práctica es que el alumno reutilice código que implementa el algoritmo A*. Se bajará código de Github, y lo utilizará para ejecutar
+este algoritmo para un grafo concreto.
+
+## Instrucciones de Uso
+
+    -Instalación de ant en maquina virtual
+    -sudo apt install ant
+    -Modificaciones en build.xml
+    -Cambio en el main 2 grafós uno dirigido y otro no
+    
+
+## Reglas del juego
+1. ¿Qué variable representa la lista ABIERTA?
+
+                La variable que representa la lista ABIERTA es openSet.
+
+2. ¿Qué variable representa la función g?
+3.              
+                La variable que representa la función g es gScore.
+
+3. ¿Qué variable representa la función f?
+            
+                La variable que representa la función f es fScore.
 
 
-Preguntas 
-¿Qué variable representa la lista ABIERTA?
-La variable que representa la lista ABIERTA es openSet.
+4. ¿Qué método habría que modificar para que la heurística representara
+la distancia aérea entre vértices?
 
-¿Qué variable representa la función g?
-La variable que representa la función g es gScore.
+                
+                El método que habría que modificar es heuristicCostEstimate().
 
-¿Qué variable representa la función f?
-La variable que representa la función f es fScore.
 
-¿Qué método habría que modificar para que la heurística representara la distancia aérea entre vértices?
-El método que habría que modificar es heuristicCostEstimate().
+5. ¿Realiza este método reevaluación de nudos cuando se encuentra una
+nueva ruta a un determinado vértice? Justifique la respuesta.
 
-¿Realiza este método reevaluación de nudos cuando se encuentra una nueva ruta a un determinado vértice? Justifique la respuesta.
-El método heuristicCostEstimate() realiza la reevaluación de nudos cuando se encuentra una nueva ruta a un determinado vértice. Este método calcula la heurística estimada desde el nodo actual hasta el destino, y si se encuentra una nueva ruta más corta, se actualiza la estimación. Esto contribuye a encontrar el camino más corto en el grafo.
+        El método heuristicCostEstimate() realiza la reevaluación de            
+        nudos cuando se encuentra una nueva ruta a un determinado               
+        vértice. Este método calcula la heurística estimada desde el            
+        nodo actual hasta el destino, y si se encuentra una nueva ruta          
+        más corta, se actualiza la estimación. Esto contribuye a                
+        encontrar el camino más corto en el grafo.
--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -1,35 +1,150 @@
-package aplicacion;
-import com.jwetherell.algorithms.graph.*;
 
-import java.util.ArrayList; 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import com.jwetherell.algorithms.data_structures.Graph;
 import com.jwetherell.algorithms.data_structures.Graph.Edge;
+import com.jwetherell.algorithms.data_structures.Graph.TYPE;
 import com.jwetherell.algorithms.data_structures.Graph.Vertex;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
-public class Main
+import com.jwetherell.algorithms.graph.*;
 
-{
+public class Main {
+    
 
+    public static void main(String[] args) {
+        // Unirected
+        UndirectedGraph grafo = new UndirectedGraph();
+        AStar<Integer> aStar = new AStar<Integer>();
+        List<Graph.Edge<Integer>> path = aStar.aStar(grafo.graph, grafo.v1, grafo.v8);
 
+        int initialNode = grafo.v1.getValue();
+        int finalNode = grafo.v8.getValue();
+        System.out.println("UNDIRECTED GRAPH: Optimal path from vertex " + initialNode + " to " + finalNode + ":");
+        for (Graph.Edge<Integer> edge : path) {
+            System.out.println(edge);
+        }
+        // Directed
+        DirectedGraph grafo2 = new DirectedGraph();
+        AStar<Integer> aStar2 = new AStar<Integer>();
+        List<Graph.Edge<Integer>> path2 = aStar2.aStar(grafo2.graph, grafo2.v1, grafo2.v8);
 
-public static void main(String[] args) {
-    // TODO code application logic here
-    UndirectedGraph g = new UndirectedGraph();
-    System.out.println(g.graph);
-    AStar<Integer> aStar = new AStar<Integer>();
-    List<Graph.Edge<Integer>> path = aStar.aStar(g.graph, g.v1, g.v5);
-    System.out.println(path);
+        int initialNode2 = grafo2.v1.getValue();
+        int finalNode2 = grafo2.v8.getValue();
+        System.out.println("DIRECTED GRAPH: Optimal path from vertex " + initialNode2 + " to " + finalNode2 + ":");
+        for (Graph.Edge<Integer> edge : path2) {
+            System.out.println(edge);
+        }
 
+    }
+   
 
-}
+        // Undirected
+    private static class UndirectedGraph {
+            final List<Vertex<Integer>> verticies = new ArrayList<Vertex<Integer>>();
+            final Graph.Vertex<Integer> v1 = new Graph.Vertex<Integer>(1);
+            final Graph.Vertex<Integer> v2 = new Graph.Vertex<Integer>(2);
+            final Graph.Vertex<Integer> v3 = new Graph.Vertex<Integer>(3);
+            final Graph.Vertex<Integer> v4 = new Graph.Vertex<Integer>(4);
+            final Graph.Vertex<Integer> v5 = new Graph.Vertex<Integer>(5);
+            final Graph.Vertex<Integer> v6 = new Graph.Vertex<Integer>(6);
+            final Graph.Vertex<Integer> v7 = new Graph.Vertex<Integer>(7);
+            final Graph.Vertex<Integer> v8 = new Graph.Vertex<Integer>(8);
+            {
+                verticies.add(v1);
+                verticies.add(v2);
+                verticies.add(v3);
+                verticies.add(v4);
+                verticies.add(v5);
+                verticies.add(v6);
+                verticies.add(v7);
+                verticies.add(v8);
+            }
+    
+            final List<Edge<Integer>> edges = new ArrayList<Edge<Integer>>();
+            final Graph.Edge<Integer> e1_2 = new Graph.Edge<Integer>(7, v1, v2);
+            final Graph.Edge<Integer> e1_3 = new Graph.Edge<Integer>(9, v1, v3);
+            final Graph.Edge<Integer> e1_6 = new Graph.Edge<Integer>(14, v1, v6);
+            final Graph.Edge<Integer> e2_3 = new Graph.Edge<Integer>(10, v2, v3);
+            final Graph.Edge<Integer> e2_4 = new Graph.Edge<Integer>(15, v2, v4);
+            final Graph.Edge<Integer> e3_4 = new Graph.Edge<Integer>(11, v3, v4);
+            final Graph.Edge<Integer> e3_6 = new Graph.Edge<Integer>(2, v3, v6);
+            final Graph.Edge<Integer> e5_6 = new Graph.Edge<Integer>(9, v5, v6);
+            final Graph.Edge<Integer> e4_5 = new Graph.Edge<Integer>(6, v4, v5);
+            final Graph.Edge<Integer> e1_7 = new Graph.Edge<Integer>(1, v1, v7);
+            final Graph.Edge<Integer> e1_8 = new Graph.Edge<Integer>(1, v1, v8);
+            {
+                edges.add(e1_2);
+                edges.add(e1_3);
+                edges.add(e1_6);
+                edges.add(e2_3);
+                edges.add(e2_4);
+                edges.add(e3_4);
+                edges.add(e3_6);
+                edges.add(e5_6);
+                edges.add(e4_5);
+                edges.add(e1_7);
+                edges.add(e1_8);
+            }
+    
+            final Graph<Integer> graph = new Graph<Integer>(verticies, edges);
+        }
+            
+    // Directed
+    private static class DirectedGraph {
+        final List<Vertex<Integer>> verticies = new ArrayList<Vertex<Integer>>();
+        final Graph.Vertex<Integer> v1 = new Graph.Vertex<Integer>(1);
+        final Graph.Vertex<Integer> v2 = new Graph.Vertex<Integer>(2);
+        final Graph.Vertex<Integer> v3 = new Graph.Vertex<Integer>(3);
+        final Graph.Vertex<Integer> v4 = new Graph.Vertex<Integer>(4);
+        final Graph.Vertex<Integer> v5 = new Graph.Vertex<Integer>(5);
+        final Graph.Vertex<Integer> v6 = new Graph.Vertex<Integer>(6);
+        final Graph.Vertex<Integer> v7 = new Graph.Vertex<Integer>(7);
+        final Graph.Vertex<Integer> v8 = new Graph.Vertex<Integer>(8);
+        {
+            verticies.add(v1);
+            verticies.add(v2);
+            verticies.add(v3);
+            verticies.add(v4);
+            verticies.add(v5);
+            verticies.add(v6);
+            verticies.add(v7);
+            verticies.add(v8);
+        }
 
-}
+        final List<Edge<Integer>> edges = new ArrayList<Edge<Integer>>();
+        final Graph.Edge<Integer> e1_2 = new Graph.Edge<Integer>(7, v1, v2);
+        final Graph.Edge<Integer> e1_3 = new Graph.Edge<Integer>(9, v1, v3);
+        final Graph.Edge<Integer> e1_6 = new Graph.Edge<Integer>(14, v1, v6);
+        final Graph.Edge<Integer> e2_3 = new Graph.Edge<Integer>(10, v2, v3);
+        final Graph.Edge<Integer> e2_4 = new Graph.Edge<Integer>(15, v2, v4);
+        final Graph.Edge<Integer> e3_4 = new Graph.Edge<Integer>(11, v3, v4);
+        final Graph.Edge<Integer> e3_6 = new Graph.Edge<Integer>(2, v3, v6);
+        final Graph.Edge<Integer> e6_5 = new Graph.Edge<Integer>(9, v6, v5);
+        final Graph.Edge<Integer> e6_8 = new Graph.Edge<Integer>(14, v6, v8);
+        final Graph.Edge<Integer> e4_5 = new Graph.Edge<Integer>(6, v4, v5);
+        final Graph.Edge<Integer> e4_7 = new Graph.Edge<Integer>(16, v4, v7);
+        final Graph.Edge<Integer> e1_8 = new Graph.Edge<Integer>(30, v1, v8);
+        {
+            edges.add(e1_2);
+            edges.add(e1_3);
+            edges.add(e1_6);
+            edges.add(e2_3);
+            edges.add(e2_4);
+            edges.add(e3_4);
+            edges.add(e3_6);
+            edges.add(e6_5);
+            edges.add(e6_8);
+            edges.add(e4_5);
+            edges.add(e4_7);
+            edges.add(e1_8);
+        }
+
+        final Graph<Integer> graph = new Graph<Integer>(Graph.TYPE.DIRECTED, verticies, edges);
+    }
+        }
+    
